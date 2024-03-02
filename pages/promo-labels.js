5ccfac86facb7df4c5050d90175f34c95f594dec
Dave <admin@devcpu.com>
2024-03-02 21:58:01 +0000

    Nearly done phase 1

--- pages/promo-labels.js
+++ pages/promo-labels.js
@@ -1,5 +1,5 @@
 import { Dropzone } from "@mantine/dropzone";
-import { Card, Image, Text, Badge, Button, Group } from '@mantine/core';
+import { Card, Image, Text, Badge, Button, Group } from "@mantine/core";
 import React, { useCallback, useState } from "react";
 import {
     cleanCsv,
@@ -8,6 +8,7 @@ import {
     splitProductGroupings,
     splitPromotions,
 } from "services/csv-parse";
+import JsBarcode from "jsbarcode";
 
 export default function PromoLabels() {
     const [promos, setPromos] = useState([]);
@@ -33,9 +34,17 @@ export default function PromoLabels() {
         reader.readAsText(files[0]);
 
         console.log(data);
-        setTimeout(() => setPromos(data), 1000);
+        setTimeout(() => {
+            setPromos(data);
+            JsBarcode(".barcode").init();
+        }, 1000);
+
+        JsBarcode(".barcode").init();
+        setTimeout(() => {
+            JsBarcode(".barcode").init();
+        }, 3000)
     });
-    console.log(promos)
+    console.log(promos);
     return (
         <div>
             <Text>Promo Labels</Text>
@@ -48,10 +57,11 @@ export default function PromoLabels() {
                 <Text ta="center">Drop stock file</Text>
             </Dropzone>
             {promos.map((promo) => (
-
                 <Card shadow="sm" padding="lg" radius="md" withBorder key={promo.id}>
                     <Card.Section>
-                        <Text fw={700} size="lg">Promotion ID: {promo.promoData.id}</Text>
+                        <Text fw={700} size="lg" padding="lg">
+                            Promotion ID: {promo.promoData.id}
+                        </Text>
                     </Card.Section>
 
                     <Group justify="space-between" mt="md" mb="xs">
@@ -59,34 +69,44 @@ export default function PromoLabels() {
                         <Badge color="pink">{promo.promoData.price}</Badge>
                     </Group>
 
-                    {promo.products.map(group => group.map(product => (
-                        <React.Fragment key={product.Barcode}>
-                            <Text size="sm" c="dimmed">
-                                {product.Description}
-                            </Text>
-                            <Text size="sm" c="dimmed">
-                                {product.Price}
-                            </Text>
-                            <Text size="sm" c="dimmed">
-                                {product.Barcode}
-                            </Text>
-                        </React.Fragment>
-                    )))}
-
-                </Card>))}
+                    {promo.products.map((group) =>
+                        group.map((product) => (
+                            <React.Fragment key={product.Barcode}>
+                                <Text size="sm" c="dimmed">
+                                    {product.Description}
+                                </Text>
+                                <Text size="sm" c="dimmed">
+                                    {product.Price}
+                                </Text>
+                                <Text size="sm" c="dimmed">
+                                    {product.Barcode}
+                                    <svg class="barcode"
+                                        jsbarcode-format="EAN13"
+                                        jsbarcode-value={product.Barcode}
+                                        jsbarcode-textmargin="0"
+                                        jsbarcode-fontoptions="bold">
+                                    </svg>
+                                </Text>
+                            </React.Fragment>
+                        ))
+                    )}
+                </Card>
+            ))}
 
             {promos.map((promo) => (
                 <div key={promo.id}>
                     <Text>{promo.promoData.id}</Text>
                     <Text>{promo.promoData.name}</Text>
                     <Text>{promo.promoData.price}</Text>
-                    {promo.products.map(group => group.map(product => (
-                        <React.Fragment key={product.Barcode}>
-                            <Text>{product.Description}</Text>
-                            <Text>{product.Price}</Text>
-                            <Text>{product.Barcode}</Text>
-                        </React.Fragment>
-                    )))}
+                    {promo.products.map((group) =>
+                        group.map((product) => (
+                            <React.Fragment key={product.Barcode}>
+                                <Text>{product.Description}</Text>
+                                <Text>{product.Price}</Text>
+                                <Text>{product.Barcode}</Text>
+                            </React.Fragment>
+                        ))
+                    )}
                 </div>
             ))}
         </div>
