fa36cb39bf2d067b74eb64047b173b9734b6cca0
Dave <admin@devcpu.com>
2024-03-04 13:39:09 +0000

    Fix build error

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,9 @@
-import Head from "next/head";
-import Header from "@components/Header";
-import Footer from "@components/Footer";
-import FeedbackForm from "@components/FeedbackForm";
-import JokeBlock from "@components/JokeBlock";
-import { Button } from "@mantine/core";
+import { Text } from "@mantine/core";
 
 export default function Home() {
   return (
     <div className="container">
-      <h1>Select a page from the sidebar</h1>
+      <Text size="xl">Select a page from the sidebar</Text>
     </div>
   );
 }
