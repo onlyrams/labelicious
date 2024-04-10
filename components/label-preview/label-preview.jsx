0afad86870e22c902124886974aa47fbd0e41f45
Dave <admin@devcpu.com>
2024-04-10 23:51:44 +0100

    Testing current layout

---
+++ components/label-preview/label-preview.jsx
@@ -0,0 +1,107 @@
+import { useEffect, useState, useRef } from "react";
+import JsBarcode from "jsbarcode";
+const template = {
+  background: {
+    // src: "/templates/promotions/promo-yellow/promo-yellow.png",
+  },
+  size: {
+    width: 670,
+    height: 112,
+  },
+  barcode: {
+    options: {
+      // format: "EAN13",
+      height: 20,
+      textMargin: 0,
+      fontSize: 12,
+      margin: 0,
+      background: "transparent",
+    },
+    xPosition: 10,
+    yPosition: 10,
+  },
+  productName: {
+    options: {
+      font: "36px sans-serif",
+      textAlign: "center",
+      fillStyle: "#000",
+    },
+    xPosition: 335,
+    yPosition: 70,
+  },
+};
+
+export default function LabelPreview(props) {
+  const [barcode, setBarcode] = useState("90453922");
+  const [productName, setProductName] = useState("Special offer");
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    if (!template || !barcode || !productName) {
+      return;
+    }
+
+    const canvas = canvasRef.current;
+    let context = canvas.getContext("2d");
+    const scale = window.devicePixelRatio;
+    canvas.width = Math.floor(template.size.width * scale);
+    canvas.height = Math.floor(template.size.height * scale);
+    context.scale(scale, scale);
+
+    let barcodeImage = new Image();
+
+    barcodeImage.onload = () => {
+      context.drawImage(
+        barcodeImage,
+        template.barcode.xPosition,
+        template.barcode.yPosition,
+        barcodeImage.width,
+        barcodeImage.height
+      );
+
+      context = Object.assign(context, template.productName.options);
+
+      context.fillText(
+        productName,
+        template.productName.xPosition,
+        template.productName.yPosition,
+        template.productName.maxWidth
+      );
+    };
+
+    if (template.background.src) {
+      let bgImage = new Image();
+      bgImage.src = template.background.src;
+      bgImage.onload = () => {
+        context.drawImage(bgImage, 0, 0, canvas.width, canvas.height);
+
+        JsBarcode(
+          barcodeImage,
+          barcode.padStart(getBarcodeType(barcode) == "EAN8" ? 8 : 13, "0"),
+          { ...template.barcode.options, format: getBarcodeType(barcode) }
+        );
+      };
+    } else {
+      JsBarcode(
+        barcodeImage,
+        barcode.padStart(getBarcodeType(barcode) == "EAN8" ? 8 : 13, "0"),
+        { ...template.barcode.options, format: getBarcodeType(barcode) }
+      );
+    }
+  }, [template, barcode, productName]);
+  return (
+    <canvas
+      height={template.size.height}
+      width={template.size.width}
+      ref={canvasRef}
+    ></canvas>
+  );
+}
+
+function getBarcodeType(barcode) {
+  if (barcode.length <= 8) {
+    return "EAN8";
+  }
+
+  return "EAN13";
+}
--- pages/print-labels.js
+++ pages/print-labels.js
@@ -1,36 +1,82 @@
-import { Button, Tabs } from "@mantine/core";
+import LabelPreview from "@components/label-preview/label-preview";
+import { Button, Paper, Space, Tabs, Text, TextInput, Title } from "@mantine/core";
 import { useState } from "react";
+
+const barcode = {
+    barcode: '123456789012',
+    labelType: 'shelf-edge',
+    template: 'medium'
+}
 
 export default function PrintLabels() {
     const [labels, setLabels] = useState([]);
+    const [currentBarcode, setCurrentBarcode] = useState("");
 
     return (
         <>
-            {labels && labels.map((label) => <div key={label}>{label}</div>)}
             <Tabs keepMounted={false} defaultValue="shelf-edge" variant="pills">
                 <Tabs.List>
                     <Tabs.Tab value="shelf-edge">Shelf Edge</Tabs.Tab>
                     <Tabs.Tab value="promotion">Promotion</Tabs.Tab>
                 </Tabs.List>
-                <br />
+
+                <Space h="md" />
+
                 <Tabs.Panel value="shelf-edge">
-                    <Tabs keepMounted={false} defaultValue="medium" variant="outline">
+                    <Tabs
+                        keepMounted={false}
+                        defaultValue="medium"
+                        orientation="vertical"
+                    >
                         <Tabs.List>
                             <Tabs.Tab value="small">Small</Tabs.Tab>
                             <Tabs.Tab value="medium">Medium</Tabs.Tab>
                             <Tabs.Tab value="large">Large</Tabs.Tab>
                         </Tabs.List>
 
-                        <Tabs.Panel value="small">
-                            Small
-                            <Button onClick={() => setLabels((prev) => [...prev, "Small"])}>Set</Button>
+                        <Tabs.Panel value="small" p="md">
+                            <TextInput
+                                label="Barcode"
+                                description="Enter or scan a barcode"
+                                value={currentBarcode}
+                                onChange={(e) => setCurrentBarcode(e.target.value)}
+                                mb="md"
+                                size="md"
+                            />
+                            <Button
+                                onClick={() => setLabels((prev) => [...prev, currentBarcode])}
+                                m={1}
+                                ml={3}
+                            >
+                                Add
+                            </Button>
                         </Tabs.Panel>
-                        <Tabs.Panel value="medium">Medium</Tabs.Panel>
-                        <Tabs.Panel value="large">Large</Tabs.Panel>
+                        <Tabs.Panel value="medium" p="md">
+                            <Button
+                                onClick={() => setLabels((prev) => [...prev, "Medium"])}
+                                m={1}
+                            >
+                                Medium
+                            </Button>
+                        </Tabs.Panel>
+                        <Tabs.Panel value="large" p="md">
+                            <Button
+                                onClick={() => setLabels((prev) => [...prev, "Large"])}
+                                m={1}
+                            >
+                                Large
+                            </Button>
+                        </Tabs.Panel>
                     </Tabs>
                 </Tabs.Panel>
-                <Tabs.Panel value="promotion">Second panel</Tabs.Panel>
+                <Tabs.Panel value="promotion">
+                    <LabelPreview />
+                </Tabs.Panel>
             </Tabs>
+            <Space h="md" />
+            <Title order={1}>Print Queue</Title>
+            <Title order={2}>{labels.length} labels</Title>
+            {labels && labels.map((label) => <Paper withBorder shadow="xs" p="md" mb="md" key={label}>{label}</Paper>)}
         </>
     );
 }
